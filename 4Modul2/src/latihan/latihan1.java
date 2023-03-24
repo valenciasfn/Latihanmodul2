399e23c18d38178335b744d546d22501aaf2ec63
valenciasfn <127365267+valenciasfn@users.noreply.github.com>
2023-03-24 10:11:35 +0700

    Update latihan1.java

--- 4Modul2/src/latihan/latihan1.java
+++ 4Modul2/src/latihan/latihan1.java
@@ -1,34 +1,36 @@
 package latihan;
-import java.util.ArrayList;
+import java.util.LinkedList;
 /**
  *
  * @author VALENCIA SEFIANA
  */
 
 public class latihan1 {
-        public static void main(String[] args) {
-            // Membuat objek ArrayList
-            ArrayList<String> hewan = new ArrayList<String>();
-            // Menambahkan elemen ke ArrayList
-            hewan.add("Sapi");
-            hewan.add("Kelinci");
-            hewan.add("Kambing");
-            hewan.add("Unta");
-            hewan.add("Domba");
+    public static void main(String[] args) {
+       // Membuat objek LinkedList Hewan dan menambahkan beberapa data
+       LinkedList<String> hewan = new LinkedList<>();
+       hewan.add("Sapi");
+       hewan.add("Kelinci");
+       hewan.add("Kambing");
+       hewan.add("Unta");
+       hewan.add("Domba");
 
-            // Menampilkan isi ArrayList 
-            System.out.println("Hewan: " + hewan);
+       System.out.println("Data Hewan : " + hewan);
 
-            //hapus index
-            ArrayList<String> delete = new ArrayList<String>();
-            String element1 = hewan.remove(1);
-            delete.add(element1);
-            String element2 = hewan.remove(2);
-            delete.add(element2);
-            String element3 = hewan.remove(1);
-            delete.add(element3);
-            System.out.println("Hewan yang dihapus : " + delete);
-            System.out.println("Output Hewan : " + hewan);
-            
-        }
+       // Membuat objek LinkedList DeleteHewan dan menambahkan beberapa data
+       LinkedList<String> deleteHewan = new LinkedList<>();
+       deleteHewan.add("Kelinci");
+       deleteHewan.add("Kambing");
+       deleteHewan.add("Unta");
+       System.out.println("Hewan yang dihapus : " + deleteHewan);
+
+       // Menghapus data pada LinkedList Hewan yang sama dengan data pada LinkedList DeleteHewan
+       for (String data : deleteHewan) 
+       {
+           hewan.remove(data);
+       }
+
+       // Menampilkan LinkedList Hewan setelah penghapusan
+       System.out.println("Output Hewan setelah dihapus : " + hewan);
+    }
 }
